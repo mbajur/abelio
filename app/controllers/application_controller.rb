12defe33787ddf981904fd496ac23763b304dd7a
mbajur <mbajur@gmail.com>
2026-05-10 15:56:31 +0200

    Update current_site method to find site by domain or fallback to first site

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -12,6 +12,6 @@ class ApplicationController < ActionController::Base
   private
 
   def current_site
-    @current_site ||= current_user.site
+    @current_site ||= Site.find_by!(domain: request.host) || Site.first
   end
 end
