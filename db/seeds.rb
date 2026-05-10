2d75a20e120c61796c975a8f40e7fcb5e4a25d02
mbajur <mbajur@gmail.com>
2026-05-10 15:47:05 +0200

    Add default template and update seed data for site creation

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,2 +1,3 @@
-site = Site.create! domain: "abelio-localhost", name: "Abelio", username: "abelio"
+template = Template.create! name: "Default", content: Rails.root.join("config", "default_template.liquid").read
+site = Site.create! domain: "abelio-localhost", name: "Abelio", username: "abelio", template: template
 User.create! email_address: "email@example.com", password: "TestPass123", site: site, username: "admin", name: "Abelio Admin"
