97991fbd608606e527e35bdd3866150b235399f5
mbajur <mbajur@gmail.com>
2026-05-10 15:50:17 +0200

    Fix seed data to use 'markup' instead of 'content' for template creation

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,3 @@
-template = Template.create! name: "Default", content: Rails.root.join("config", "default_template.liquid").read
+template = Template.create! name: "Default", markup: Rails.root.join("config", "default_template.liquid").read
 site = Site.create! domain: "abelio-localhost", name: "Abelio", username: "abelio", template: template
 User.create! email_address: "email@example.com", password: "TestPass123", site: site, username: "admin", name: "Abelio Admin"
