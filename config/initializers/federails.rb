619c000a4edb86336dceec1391cdf85de4a0d27d
mbajur <mbajur@gmail.com>
2026-05-11 19:28:55 +0200

    Undo Anonunce

--- config/initializers/federails.rb
+++ config/initializers/federails.rb
@@ -13,6 +13,7 @@ Rails.application.config.after_initialize do
   # Fediverse::Inbox.register_handler("Update", "*", ActivityPub::ActorActivityHandler, :handle_update_activity)
   Fediverse::Inbox.register_handler("Like", "*", Federation::LikeActivityHandler, :handle_like_activity)
   Fediverse::Inbox.register_handler("Undo", "Like", Federation::UndoLikeActivityHandler, :handle_undo_like_request)
+  Fediverse::Inbox.register_handler("Undo", "Announce", Federation::UndoAnnounceActivityHandler, :handle_undo_announce_request)
   Fediverse::Inbox.register_handler("Announce", "*", Federation::AnnounceActivityHandler, :handle_announce_activity)
   # Fediverse::Inbox.register_handler("QuoteRequest", "*", ActivityPub::QuoteRequestHandler, :handle_quote_request)
 end
