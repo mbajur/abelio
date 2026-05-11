994d8f23570e8712c39c5d643d13c5c4516ba94e
mbajur <mbajur@gmail.com>
2026-05-11 15:10:23 +0200

    Refactor handle_undo_like_request method for improved clarity and safety

--- app/services/federation/undo_like_activity_handler.rb
+++ app/services/federation/undo_like_activity_handler.rb
@@ -1,18 +1,20 @@
 class Federation::UndoLikeActivityHandler
-  def self.handle_undo_like_request(activity_hash_or_id)
-    activity = Fediverse::Request.dereference(activity_hash_or_id)
-    actor = Federails::Actor.find_or_create_by_object activity["actor"]
-    object = Fediverse::Request.dereference(activity.dig("object", "object"))
+  def self.handle_undo_like_request(activity)
+    original_activity = Request.dereference(activity["object"])
+    actor  = Federails::Actor.find_or_create_by_object original_activity["actor"]
 
-    object_id = object["id"]
+    object_id = original_activity["object"]
     raise "Not a local ID" unless Federails::Utils::Host.local_url?(object_id)
 
     local_route = Federails::Utils::Host.local_route(object_id)
+    Rails.logger.info local_route
     raise ActiveRecord::RecordNotFound unless local_route[:controller] == "federails/server/published" && local_route[:action] == "show" && local_route[:publishable_type] == "posts"
 
     entity = Post.find(local_route[:id])
+    Rails.logger.info entity.as_json
     like = Federails::Activity.find_by actor: actor, entity: entity, action: "Like"
-    like&.destroy
+    Rails.logger.info like.as_json
+    like&.destroy!
 
     entity.update_likes_count!
   end
