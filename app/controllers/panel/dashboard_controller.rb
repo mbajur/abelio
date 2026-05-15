066298b5748c6f42cd0026c4432ae8652c915d3e
mbajur <mbajur@gmail.com>
2026-05-15 10:41:35 +0200

    Include posts by actors current site follows (dashboard)

--- app/controllers/panel/dashboard_controller.rb
+++ app/controllers/panel/dashboard_controller.rb
@@ -1,7 +1,9 @@
 module Panel
   class DashboardController < PanelController
     def index
-      @posts = Post.non_sketches.published.freshly_published_first.includes(:federails_actor)
+      @posts = Post
+        .by_site_and_its_followings(Current.site)
+        .non_sketches.published.freshly_published_first
     end
   end
 end
